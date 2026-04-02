cf5116556d2eb3bc39a145d48e427d7d59eb5d2e
NambiLucia <nambilucia@gmail.com>
2026-04-02 11:21:03 +0300

    feat:add register user function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,22 +1,43 @@
 import {prisma} from "../lib/prisma"
 import { Request,Response } from "express";
+import bcrypt from "bcrypt";
 
 
 export const register=async(req:Request,res:Response)=>{
     try{
         const {full_name,email,password} =req.body;
+       
+const existingUser = await prisma.user.findUnique({
+    where:{email}
+})
+if(existingUser){
+     return res.status(400).json({ message: "Email already in use" });
+}
+
+const hashedPassword= await bcrypt.hash(password,10)
+
         const newUser=await prisma.user.create({
             data:{
                 full_name,
                 email,
-                password
+                password: hashedPassword,
             }
         })
-        return res.status(200).json({message:"User created successfully",data:newUser})
+        return res.status(201).json({
+             message: "User created successfully",
+ 
+      user: {
+        id: newUser.id,
+        full_name: newUser.full_name,
+        email: newUser.email,
+        role: newUser.role,
+      }
+
+        })
     }
     catch(error){
-        console.error(error)
-        return res.status(500).json("Error creating user")
+         console.error("Error creating user:", error);
+       return res.status(500).json({ error: "Failed to create user" });
 
     }
 }
