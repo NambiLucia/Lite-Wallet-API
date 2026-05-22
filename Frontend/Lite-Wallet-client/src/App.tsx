487e46f6d1df40fb0f105f12f9dc07801db8eb54
NambiLucia <nambilucia@gmail.com>
2026-05-22 16:06:06 +0300

    chore:frontend set up

---
+++ Frontend/Lite-Wallet-client/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Lite Wallet</h1>
+      <p>Frontend is working!</p>
+    </div>
+  )
+}
+
+export default App
