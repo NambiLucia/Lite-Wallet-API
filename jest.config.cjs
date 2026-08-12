1e9ff3f19498debb10d6e210b133dd69f22c3afd
NambiLucia <nambilucia@gmail.com>
2026-08-12 14:36:22 +0300

    Complete Lite Wallet API tests and deployment setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,27 @@
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+// };
+
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+
+  extensionsToTreatAsEsm: [".ts"],
+
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.json",
+      },
+    ],
+  },
+
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
